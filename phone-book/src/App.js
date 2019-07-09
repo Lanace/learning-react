4af47dba2b327dd32e9f0061ea49acb7f46ffcde
JeonIlju <lanace93@gmail.com>
2019-07-09 15:12:05 +0900

    배열로 Key를 관리하는 방법을 추가

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 import './App.css';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '사람1',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '사람2',
+        phone: '010-1234-5678'
+      }
+    ]
+  }
+
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({
+        id: this.id++,
+        ...data
+      })
+    })
   }
 
   render() {
+    const {information} = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate}/>
+        <PhoneInfoList data={information}></PhoneInfoList>
       </div>
     );
   }
